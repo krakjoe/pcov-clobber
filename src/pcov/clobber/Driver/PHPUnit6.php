191f54df9a60fc38f1891248d9e3220121a43e6b
Joe Watkins <krakjoe@php.net>
2019-08-02 06:24:11 +0200

    apply patches from php-code-coverage to clobber driver

--- src/pcov/clobber/Driver/PHPUnit6.php
+++ src/pcov/clobber/Driver/PHPUnit6.php
@@ -8,39 +8,37 @@ namespace pcov\Clobber\Driver
 	 */
 	class PHPUnit6 implements \SebastianBergmann\CodeCoverage\Driver\Driver
 	{
-	    public function __construct($filter = null)
-	    {
+		public function __construct($filter = null)
+		{
 		
-	    }
-
-	    /**
-	     * Start collection of code coverage information.
-	     */
-	    public function start($determineUnusedAndDead = true)
-	    {
-		\pcov\start();
-	    }
-
-	    /**
-	     * Stop collection of code coverage information.
-	     */
-	    public function stop()
-	    {
-		\pcov\stop();
-
-		$waiting = \pcov\waiting();
-		$collect  = [];
-
-		if ($waiting) {
-			$collect = \pcov\collect(\pcov\inclusive, $waiting);
-
-			if ($collect) {
-				\pcov\clear();
-			}
 		}
 
-		return $collect;
-	    }
+		/**
+		 * Start collection of code coverage information.
+		 */
+		public function start($determineUnusedAndDead = true)
+		{
+			\pcov\start();
+		}
+
+		/**
+		 * Stop collection of code coverage information.
+		 */
+		public function stop()
+		{
+			\pcov\stop();
+
+			$waiting = \pcov\waiting();
+			$collect  = [];
+
+			if ($waiting) {
+				$collect = \pcov\collect(\pcov\inclusive, $waiting);
+
+				\pcov\clear();
+			}
+
+			return $collect;
+		}
 	}
 
 }
