f49cb318b117a58e32c72be24fec0f54bdaf35bb
Tamas Kalman <tkalman@brightidea.com>
2019-10-27 03:52:41 -0700

    phpunit5 support

---
+++ src/pcov/clobber/Driver/PHPUnit5.php
@@ -0,0 +1,44 @@
+<?php
+namespace pcov\Clobber\Driver
+{
+	/**
+	 * Driver for PCOV code coverage functionality.
+	 *
+	 * @codeCoverageIgnore
+	 */
+	class PHPUnit5 implements \SebastianBergmann\CodeCoverage\Driver\Driver
+	{
+		public function __construct($filter = null)
+		{
+		
+		}
+
+		/**
+		 * Start collection of code coverage information.
+		 */
+		public function start($determineUnusedAndDead = true)
+		{
+			\pcov\start();
+		}
+
+		/**
+		 * Stop collection of code coverage information.
+		 */
+		public function stop()
+		{
+			\pcov\stop();
+
+			$waiting = \pcov\waiting();
+			$collect  = [];
+
+			if ($waiting) {
+				$collect = \pcov\collect(\pcov\inclusive, $waiting);
+
+				\pcov\clear();
+			}
+
+			return $collect;
+		}
+	}
+
+}
